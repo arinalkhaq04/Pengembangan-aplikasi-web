07be0451e35dd2c23c50a4d19da79865ae760fa8
arinalkhaq04 <111882707+arinalkhaq04@users.noreply.github.com>
2022-08-30 23:40:43 +0700

    Add files via upload

---
+++ output pertemuan2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Output Pertemuan 2</title>
+</head>
+<body>
+<?php
+	function NamaKeluarga($nama, $alamat, $tgllahir){
+		echo "$nama.$alamat.$tgllahir<br>";			
+	}
+	NamaKeluarga("Arinal","Lamongan","04/01/2000");
+	NamaKeluarga("Fauzi","Lamongan","04/04/1987");
+	NamaKeluarga("latifah","Lamongan","23/02/1984");
+?>
+</body>
+</html>
